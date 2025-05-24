5a7ed7bcf0f20c41227d48956695505a1ad4d0a7
whgkswo <gjuk7238@gmail.com>
2025-05-25 00:54:36 +0900

    1. 회원가입

--- src/main/java/net/whgkswo/stonesmith/entities/members/MemberService.java
+++ src/main/java/net/whgkswo/stonesmith/entities/members/MemberService.java
@@ -2,6 +2,7 @@ package net.whgkswo.stonesmith.entities.members;
 
 import net.whgkswo.stonesmith.entities.members.rank.Rank;
 import net.whgkswo.stonesmith.entities.members.rank.RankRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,18 +10,21 @@ public class MemberService {
     private MemberMapper memberMapper;
     private MemberRepository memberRepository;
     private RankRepository rankRepository;
+    private PasswordEncoder passwordEncoder;
 
-    public MemberService(MemberMapper memberMapper, MemberRepository memberRepository, RankRepository rankRepository){
+    public MemberService(MemberMapper memberMapper, MemberRepository memberRepository, RankRepository rankRepository, PasswordEncoder passwordEncoder){
         this.memberMapper = memberMapper;
         this.memberRepository = memberRepository;
         this.rankRepository = rankRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public Member createUser(MemberDto dto){
         Member member = memberMapper.dtoToUser(dto);
 
-        // TODO: 암호화 적용
-        member.setPassword(dto.rawPassword());
+        // 비밀번호 암호화 적용
+        String encryptedPassword = passwordEncoder.encode(dto.rawPassword());
+        member.setPassword(encryptedPassword);
 
         Rank rank = new Rank(Rank.Tier.BRONZE, 10);
         member.setRank(rank);
