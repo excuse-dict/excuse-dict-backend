0ea9e474791ad278a920b1ca9c2bff52aadaadb6
whgkswo <gjuk7238@gmail.com>
2025-06-05 22:42:11 +0900

    1. 서버 실행시 db와 es데이터 갯수 다르면 동기화

--- src/main/java/net/whgkswo/excuse_bundle/elasticsearch/ElasticsearchDataInitializer.java
+++ src/main/java/net/whgkswo/excuse_bundle/elasticsearch/ElasticsearchDataInitializer.java
@@ -1,6 +1,7 @@
 package net.whgkswo.excuse_bundle.elasticsearch;
 
 import lombok.RequiredArgsConstructor;
+import net.whgkswo.excuse_bundle.entities.posts.tags.entities.Tag;
 import net.whgkswo.excuse_bundle.entities.posts.tags.entities.TagDocument;
 import net.whgkswo.excuse_bundle.entities.posts.tags.repositories.TagSearchRepository;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
@@ -25,18 +26,32 @@ public class ElasticsearchDataInitializer {
 
     private void syncTags(){
         try {
-            if (tagSearchRepository.count() == 0) {
-                String sql = "SELECT id, value, category FROM tag";
-                List<TagDocument> documents = jdbcTemplate.query(sql, (rs, rowNum) ->
-                        new TagDocument(
-                                rs.getString("value"),
-                                rs.getString("category"),
-                                rs.getLong("id")
-                        )
-                );
-                tagSearchRepository.saveAll(documents);
+            // DB 개수 확인
+            Integer dbCount = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM tag", Integer.class);
+
+            // ES 개수 확인
+            long esCount = tagSearchRepository.count();
+
+            // 다르면 동기화
+            if(dbCount.longValue() == esCount) {
+                return;
             }
+
+            tagSearchRepository.deleteAll();
+
+            String sql = "SELECT id, value, category FROM tag";
+            List<TagDocument> documents = jdbcTemplate.query(sql, (rs, rowNum) ->
+                    new TagDocument(
+                            rs.getString("value"),
+                            rs.getString("category"),
+                            rs.getLong("id")
+                    )
+            );
+
+            tagSearchRepository.saveAll(documents);
+
         } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
