3998904663fd7aa135df059544859c002c6c473e
whgkswo <gjuk7238@gmail.com>
2025-09-15 16:27:09 +0900

    1. 프롬프트 수정

--- src/main/java/net/whgkswo/excuse_bundle/entities/excuses/controller/ExcuseController.java
+++ src/main/java/net/whgkswo/excuse_bundle/entities/excuses/controller/ExcuseController.java
@@ -3,8 +3,10 @@ package net.whgkswo.excuse_bundle.entities.excuses.controller;
 import lombok.RequiredArgsConstructor;
 import net.whgkswo.excuse_bundle.gemini.prompt.PromptBuilder;
 import net.whgkswo.excuse_bundle.gemini.service.GeminiService;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
@@ -20,7 +22,7 @@ public class ExcuseController {
     private final PromptBuilder promptBuilder;
 
     @GetMapping("/generate")
-    public Mono<String> generateExcuse(String situation){
+    public Mono<String> generateExcuse(@RequestParam @Length(min = 5, max = 100, message = "상황은 5~100자로 입력해주세요.") String situation){
 
         String prompt = promptBuilder.buildExcusePrompt(situation);
 
