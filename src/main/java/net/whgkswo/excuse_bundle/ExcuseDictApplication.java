f5e83eac37174799bed24dd87e7a321a3e41bab6
whgkswo <gjuk7238@gmail.com>
2025-09-24 20:43:24 +0900

    1. 서버 시작시 타임존 강제 고정

--- src/main/java/net/whgkswo/excuse_bundle/ExcuseDictApplication.java
+++ src/main/java/net/whgkswo/excuse_bundle/ExcuseDictApplication.java
@@ -1,14 +1,23 @@
 package net.whgkswo.excuse_bundle;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableAspectJAutoProxy
 @EnableScheduling
 public class ExcuseDictApplication {
+
+    // 서버 타임존 고정
+    @PostConstruct
+    public void init(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExcuseDictApplication.class, args);
