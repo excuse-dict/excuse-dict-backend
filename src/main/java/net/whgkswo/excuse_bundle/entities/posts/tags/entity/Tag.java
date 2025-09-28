420efe25a500a4ffd2ec904856f53c52291c93fb
whgkswo <gjuk7238@gmail.com>
2025-09-29 04:50:44 +0900

    1. 태그 데이터 추가

--- src/main/java/net/whgkswo/excuse_bundle/entities/posts/tags/entity/Tag.java
+++ src/main/java/net/whgkswo/excuse_bundle/entities/posts/tags/entity/Tag.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 import net.whgkswo.excuse_bundle.entities.BaseEntity;
 import net.whgkswo.excuse_bundle.lib.json.StringJsonToSetConverter;
+import org.hibernate.validator.constraints.UniqueElements;
 
 import java.util.Set;
 
@@ -12,6 +13,7 @@ import java.util.Set;
 @Getter
 @Setter
 public class Tag extends BaseEntity {
+    @Column(unique = true)
     private String value;
 
     @Enumerated(EnumType.STRING)
@@ -34,8 +36,9 @@ public class Tag extends BaseEntity {
         HEALTH(Set.of("건강", "병원", "아픔", "몸", "치료", "의료", "진료", "진찰", "시술", "수술", "컨디션")),
         HOME_FACILITY(Set.of("집", "주거", "주택", "설비", "수리", "고장", "문제", "이상", "불량", "결함", "공사", "작동", "안됨", "안나옴", "끊김")),
         LOVE(Set.of("연애", "사랑", "썸", "남자친구", "여자친구", "남친", "여친", "애인", "교제", "만남")),
-        RELIGIOUS(Set.of("종교", "신", "신앙", "수양", "믿음")),
+        RELIGIOUS(Set.of("종교", "신", "신앙", "수양", "믿음", "선행", "주님")),
         STUDY(Set.of("학교", "자격증", "취업", "공무원", "자기계발", "합격")),
+        SUPERNATURAL(Set.of("심령", "귀신", "무서움", "공포", "유령", "초자연", "오컬트", "미스터리")),
         TRANSPORT(Set.of("교통", "차", "도로", "이동", "운송", "통행")),
         WEATHER(Set.of("날씨", "기상", "기후", "천재지변", "자연재해")),
         ETC(Set.of("기타", "개인사", "사정"))
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,1 +1,2 @@
-CREATE INDEX idx_net_votes ON post((upvote_count - downvote_count) DESC);
+--CREATE INDEX IF NOT EXISTS idx_net_votes ON post((upvote_count - downvote_count) DESC);
+SELECT 1; -- 빈 sql문 에러 방지용
